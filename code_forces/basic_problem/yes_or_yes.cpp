9adf16a0b83048bc5f8b859b655444acf248f56e
umarfaruk2 <farukdev22@gmail.com>
2024-01-14 22:32:57 +0600

    solve new problem

--- code_forces/basic_problem/yes_or_yes.cpp
+++ code_forces/basic_problem/yes_or_yes.cpp
@@ -12,30 +12,19 @@ int main() {
         string s;
         cin >> s;
 
-        string new_string = "esy";
-        int arr[27] = {0};
+        string new_string = "yes";
 
         for(int i = 0; i < 3; i++) {
             if(s[i] < 'a') {
                 s[i] = s[i] + 32;
-                arr[s[i] - 'a']  = 1; 
             }
-            arr[s[i] - 'a']  = 1; 
         }
-
-        sort(s.begin(), s.end());
 
         if(s == new_string) {
             cout << "YES" << "\n";
         } else {
             cout << "NO" << "\n";
         }
-
-        for(int i = 0; i < 27; i++) {
-            cout << i << " -> " << arr[i] << "\n";
-        }
-
-        cout << "\n";
     } 
     return 0;
 }
