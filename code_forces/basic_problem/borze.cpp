4e0e9181184adb3ac442b1e0fce58ba33295b78e
umarfaruk2 <farukdev22@gmail.com>
2024-02-08 16:19:54 +0600

    problem solve

--- code_forces/basic_problem/borze.cpp
+++ code_forces/basic_problem/borze.cpp
@@ -5,6 +5,22 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    
+    string s;
+    cin >> s;
+
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == '-') {
+            if(s[i + 1] == '.') {
+                cout << 1;
+                i++;
+            } else if(s[i + 1] == '-') {
+                cout << 2;
+                i++;
+            }
+        } else if(s[i] == '.') {
+            cout << 0;
+        }
+    }
+    cout << "\n";
     return 0;
 }
